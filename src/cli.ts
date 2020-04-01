4fa1d62a963b7733cda8fa292d5f2cd89cc6c2c7
Sebastian Speitel <sebastian.speitel@outlook.de>
2020-04-01 17:08:26 +0200

    feat: uninstall in cli

--- src/cli.ts
+++ src/cli.ts
@@ -6,23 +6,36 @@ const manager = new DatapackManager();
 manager;
 
 yargs
-  .command("install <pack> <world>", "Install a datapack", yargs => {
-    const args = yargs.options({
+  .command(["install <pack> <world>", "i"], "Install a datapack", yargs => {
+    const argv = yargs.options({
       mode: {
         desc: "Install mode",
         alias: "m",
         choices: ["symlink", "copy", "compile", "move"] as InstallMode[]
       }
-    });
+    }).argv;
 
     const {
       mode,
       _: [, _pack, _world]
-    } = args.argv;
+    } = argv;
 
     const world = new World(_world);
 
     world.install(_pack, { mode }).catch(e => {
+      console.error(e?.message ?? e);
+    });
+  })
+  .command(["uninstall <pack> <world>", "u"], "Uninstall a datapack", yargs => {
+    const argv = yargs.argv;
+
+    const {
+      _: [, _pack, _world]
+    } = argv;
+
+    const world = new World(_world);
+
+    world.uninstall(_pack).catch(e => {
       console.error(e?.message ?? e);
     });
   })
